39f48de27de976c01024a4a4947fbc84117253bf
ricafrentedrea <ricafrentedrea@gmail.com>
2019-03-26 14:44:18 +0800

    New upload for Shopping-List repo

---
+++ script.js
@@ -0,0 +1,92 @@
+const button = document.getElementById("enter");
+const input = document.getElementById("userinput");
+const ul = document.querySelector("ul");
+
+// Create close button
+var myNodelist = document.getElementsByTagName("LI");
+var i;
+for (i = 0; i < myNodelist.length; i++) {
+		var span = document.createElement("SPAN");
+		var txt = document.createTextNode("\u00D7");
+		span.className = "close";
+		span.appendChild(txt);
+		myNodelist[i].appendChild(span);
+}
+
+
+// Click on a close button to hide the current list item
+var close = document.getElementsByClassName("close");
+var i;
+for (i = 0; i < close.length; i++) {
+close[i].onclick = function() {
+	var div = this.parentElement;
+	div.style.display = "none";
+}
+}
+
+function inputLength() {
+	return input.value.length;
+}
+
+function createListElement() {
+	const li = document.createElement("LI");
+	li.appendChild(document.createTextNode(input.value));
+	ul.appendChild(li);
+	input.value = "";
+	
+	var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  li.appendChild(span);
+
+  for (i = 0; i < close.length; i++) {
+    close[i].onclick = function() {
+      var div = this.parentElement;
+      div.style.display = "none";
+    }
+  }
+}
+
+function addListAfterClick() {
+	if (inputLength() > 0) {
+		createListElement();
+	}
+}
+
+function addListAfterKeypress(event) {
+	if (inputLength() > 0 && event.keyCode === 13) {
+		createListElement();
+	}
+}
+
+button.addEventListener("click", addListAfterClick);
+
+input.addEventListener("keypress", addListAfterKeypress);
+
+
+
+
+// toggle done/undone
+ul.addEventListener('click', function(ev) {
+	if (ev.target.tagName === 'LI') {
+	  ev.target.classList.toggle('done');
+	}
+  }, false);
+
+
+// for text field stick
+var contScroll = document.getElementsByClassName("container");
+
+contScroll.onscrollonscroll = function() {myFunction()};
+
+var header = document.getElementById("myHeader");
+var sticky = header.offsetTop;
+
+function myFunction() {
+  if (contScroll.pageYOffset > sticky) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+}
